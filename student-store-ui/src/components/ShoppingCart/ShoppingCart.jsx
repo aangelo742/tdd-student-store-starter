c67de85db7dc6fcf95d16c150e09fa7a976b2103
Angelo's Laptop <aaneglo742@gmail.com>
2022-06-19 22:39:49 -0500

    Added ShoppingCart.jsx

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,7 +2,61 @@ import * as React from "react"
 import "./ShoppingCart.css"
 
 export default function ShoppingCart(props) {
-    return (
-        <div></div>
-    )
+    let subtotal = 0;
+    console.log(props.shoppingCart.length)
+    if(props.shoppingCart.length === 0) {
+        return (
+            <div className="shopping-cart">
+                <div className="title">
+                    <h3>Shopping Cart</h3>
+                </div>
+                <div className="notification">
+                    No items added to cart yet. Start shopping now!
+                </div>
+            </div>
+        )
+    }
+    else if(props.shoppingCart.length > 0) {
+        console.log("Products: ", props.products.products)
+        return (
+            <div className="shopping-cart">
+                <div className="title">
+                    <h3>Shopping Cart</h3>
+                </div>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Unit Price</th>
+                        <th>Cost</th>
+                    </tr>
+                    {
+                        props.shoppingCart.map((item) => {
+                            console.log("Item Product #: ", item.product)
+                            console.log("Quantity: ", item.quantity)
+                            let product = props.products.products[item.product - 1]
+                            console.log("Actual Product: ", product)
+                            let itemTotal = (product.price * item.quantity).toFixed(2)
+                            console.log("Item Total Price: ", itemTotal)
+                            subtotal = subtotal + parseInt(itemTotal)
+                            console.log("Subtotal: ", subtotal)
+                            return (
+                                <tr>
+                                    <td>{product.name}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>{product.price}</td>
+                                    <td>${itemTotal}</td>
+                                </tr>
+                            )           
+                        })
+                    }
+                    
+                </table>
+            </div>
+        )
+    } else {
+        return null
+    }
+
 }
+
--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -3,17 +3,22 @@ import "./Sidebar.css"
 import ShoppingCart from "../ShoppingCart/ShoppingCart"
 export default function Sidebar(props) {
   if(!props.isOpen){
+    console.log(props.shoppingCart)
     return (
-      <section className="sidebar closed">
-        <button className="toggle-button" onClick={props.handleOnToggle}>&rarr;</button>
-        <ShoppingCart isOpen={props.isOpen} products={props.products} shoppingCart={props.shoppingCart} />
+      <section className="sidebar">
+        <div className="closed">
+          <button className="toggle-button" onClick={props.handleOnToggle}>&rarr;</button>
+        </div>
       </section>
     )
   } else if(props.isOpen){
+    console.log(props.shoppingCart)
     return (
-      <section className="sidebar open">
-        <button className="toggle-button" onClick={props.handleOnToggle}>&larr;</button>
-        <ShoppingCart isOpen={props.isOpen} products={props.products} shoppingCart={props.shoppingCart} />
+      <section className="sidebar">
+        <div className="opened">
+          <button className="toggle-button" onClick={props.handleOnToggle}>&larr;</button>
+          <ShoppingCart isOpen={props.isOpen} products={props.products} shoppingCart={props.shoppingCart} />
+        </div>
       </section>
     )
   }
